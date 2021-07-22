d2f24f203f8914d99b9955e9986b797ffb278ab8
Felipe <felipelagocoimbra@hotmail.com>
2021-07-22 09:26:50 -0300

    Melhorias no código

--- JavaScript.js
+++ JavaScript.js
@@ -1,20 +1,27 @@
-function insert(num){
-   let numero = document.getElementById('resultado').innerHTML
-    document.getElementById('resultado').innerHTML = numero + num;
-}
-function clean(){
-    document.getElementById('resultado').innerHTML = "";
-}
-function back(){
-    let resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length -1);
-}
-function calcular(){
-    let resultado = document.getElementById('resultado').innerHTML;
-    if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado);
+class Calculadora {
+
+    constructor() {
     }
-    else{
-        document.getElementById('resultado').innerHTML = "Nada para calcular"
+
+    insert(num) {
+        let numero = document.getElementById('resultado').innerHTML
+        document.getElementById('resultado').innerHTML = numero + num;
+    }
+    clean() {
+        document.getElementById('resultado').innerHTML = "";
+    }
+    back() {
+        let backspace = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = backspace.substring(0, backspace.length - 1);
+    }
+    calcular() {
+        let resultado = document.getElementById('resultado').innerHTML;
+        if (resultado) {
+            document.getElementById('resultado').innerHTML = eval(resultado);
+        }
+        else {
+            document.getElementById('resultado').innerHTML = "Nada para calcular"
+        }
     }
 }
+let calculadora = new Calculadora();
